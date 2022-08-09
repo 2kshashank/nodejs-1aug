b4cc95a1da94ce8d740d8efa5046072406288911
Admin <admin@example.com>
2022-08-09 06:20:34 +0100

    Get and Post using mongodb

--- router/products.mjs
+++ router/products.mjs
@@ -1,20 +1,59 @@
+import { ObjectID } from "bson";
 import express from "express";
-
+import { createClient } from "../utils/db.mjs"
 const Router = express.Router();
 
-// /products + /
-// /products/
-// /products
 
-Router.get("/", (request, response) => {
-  response.send("This is the products page.");
+// GET /products/ ✔
+// GET /products/:id ✔
+// POST /products/
+// DELETE /products/:id
+// PUT /products/:id
+
+
+Router.get("/", async (request, response) => {
+    const client = createClient();
+    await client.connect();
+    const db = client.db("edureka");
+    const collection = db.collection("products");
+    const docs = await collection.find().toArray();
+    client.close()
+
+    response.json(docs);
+
 });
 
-// /products/laptop/
-// /products/laptop
-Router.get("/laptop", (request, response) => {
-    response.send("This is the laptop page.");
-  });
+Router.get("/:id", async (request, response) => {
+
+  const id = request.params.id;
+
+  const client = createClient();
+  await client.connect();
+  const db = client.db("edureka");
+  const collection = db.collection("products");
+  const docs = await collection.findOne({ _id : ObjectID(id) });
+  client.close()
+
+  response.json(docs);
+
+});
+
+Router.post("/", async (request, response) => {
+
+  const body = request.body
+
+  const client = createClient();
+  await client.connect();
+  const db = client.db("edureka");
+  const collection = db.collection("products");
+  const insertedDoc = await collection.insertOne(body)
+  client.close()
+
+  response.json(insertedDoc);
+
+});
+
+
   
 
 export default Router;
--- router/users.mjs
+++ router/users.mjs
@@ -1,13 +1,25 @@
 import express, { json } from "express";
 import axios from "axios";
+import { MongoClient } from "mongodb";
+
+const dbUrl = "mongodb://127.0.0.1:27017"
+const client = new MongoClient(dbUrl);
 
 const Router = express.Router();
-
 Router.get("/", async (req, res) => {
     try {
-        const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+        // const response = await axios.get("https://jsonplaceholder.typicode.com/users");
         // res.json(response.data);
-        res.render("users", { users : response.data })
+        await client.connect();
+        const db = client.db("edureka");
+        const collection = db.collection("users");
+
+        const docs = await collection.find().toArray()
+
+        // res.render("users", { users : docs })
+
+        res.json(docs)
+
     } catch (error) {
         res.status(500).send("Internal server error.");
     }

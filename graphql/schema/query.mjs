fa74d99a641391ff3f5572b364eaf53f05f496ab
Admin <admin@example.com>
2022-08-10 07:01:18 +0100

    Added args.

--- graphql/schema/query.mjs
+++ graphql/schema/query.mjs
@@ -7,7 +7,8 @@ const UserType = new GraphQLObjectType({
         firstName : { type : GraphQLString },
         lastName : { type : GraphQLString },
         email : { type : GraphQLString },
-        username : { type : GraphQLString }
+        username : { type : GraphQLString },
+        _id : { type : GraphQLString }
     }
 })
 
@@ -18,6 +19,13 @@ const Query = new GraphQLObjectType({
             type : GraphQLList(UserType),
             resolve : ()=>{
                 return UserModel.find()
+            }
+        },
+        user : {
+            type : UserType,
+            args : { id : { type : GraphQLString } },
+            resolve : (context, args)=>{
+                return UserModel.findById(args.id)
             }
         }
     }

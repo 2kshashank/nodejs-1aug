f9766dc393bc4406332fc1ad573df2fc94980427
Admin <admin@example.com>
2022-08-10 06:02:55 +0100

    Added add user form.

--- models/users.mjs
+++ models/users.mjs
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 const UserSchema = mongoose.Schema({
-    name : String,
-    age : Number,
-    location : String
+    firstName : String,
+    lastName : String,
+    username : String,
+    email : String
 })
 
 export const UserModel = mongoose.model("Users", UserSchema, "users")
--- router/users.mjs
+++ router/users.mjs
@@ -5,14 +5,33 @@ import { UserModel } from "../models/users.mjs"
 const Router = express.Router();
 Router.get("/", async (req, res) => {
     try {
-        
         const users = await UserModel.find();
-        res.json(users);
-
+        // res.json(users);
+        res.render("users", { users : users });
     } catch (error) {
+        console.log(error);
         res.status(500).send("Internal server error.");
-    }
-    
+    } 
+});
+
+Router.get("/add-user", (req, res)=>{
+    res.render("add-user");
+})
+
+// /users
+Router.post("/add-user", async (request, response) => {
+    try {
+        const body = request.body;
+        
+        const newUser = new UserModel(body)
+
+        await newUser.save()
+        
+        response.redirect("/users")
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error.");
+    } 
 });
 
 export default Router;

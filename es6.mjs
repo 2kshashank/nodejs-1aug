df2434467a3fa4f5e671742c25fef208c0dc0800
Admin <admin@example.com>
2022-08-11 06:16:20 +0100

    const and let.

---
+++ es6.mjs
@@ -0,0 +1,43 @@
+var a = "abc";
+let nameUsingLet = "qwerty"
+const nameUsingConst = "qweery"
+a = "def";
+console.log(a)
+
+if(1 === 1) {
+    nameUsingLet = "daskjdhskajdhjsad"
+    // nameUsingConst = "dasdsadsad"
+    const xyz = 12312313
+}
+
+console.log(xyz);
+
+console.log(nameUsingLet)
+
+
+
+
+
+// if(true) {
+//     var b = "qwerty"
+//     let bInsideIfCondition = "eruoiewuroqwer"
+// }
+
+// for (let index = 0; index < 3; index++) {
+//     setTimeout(()=>{ console.log(index) }, 0)
+// }
+
+// if(true)
+//     {
+//         var b = "ruweorwqiur"
+//     }
+
+// function c(){
+//     var name = "praveen"
+// }
+// c();
+
+// console.log(b)
+// // console.log(name)
+// console.log(nameUsingLet);
+// console.log(bInsideIfCondition);

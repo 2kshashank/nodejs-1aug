badc81634d31bab71729365d80d6ab50f022a48a
Admin <admin@example.com>
2022-08-11 06:16:37 +0100

    commented let and const

--- es6.mjs
+++ es6.mjs
@@ -1,43 +1,43 @@
-var a = "abc";
-let nameUsingLet = "qwerty"
-const nameUsingConst = "qweery"
-a = "def";
-console.log(a)
+// var a = "abc";
+// let nameUsingLet = "qwerty"
+// const nameUsingConst = "qweery"
+// a = "def";
+// console.log(a)
 
-if(1 === 1) {
-    nameUsingLet = "daskjdhskajdhjsad"
-    // nameUsingConst = "dasdsadsad"
-    const xyz = 12312313
-}
-
-console.log(xyz);
-
-console.log(nameUsingLet)
-
-
-
-
-
-// if(true) {
-//     var b = "qwerty"
-//     let bInsideIfCondition = "eruoiewuroqwer"
+// if(1 === 1) {
+//     nameUsingLet = "daskjdhskajdhjsad"
+//     // nameUsingConst = "dasdsadsad"
+//     const xyz = 12312313
 // }
 
-// for (let index = 0; index < 3; index++) {
-//     setTimeout(()=>{ console.log(index) }, 0)
-// }
+// console.log(xyz);
 
-// if(true)
-//     {
-//         var b = "ruweorwqiur"
-//     }
+// console.log(nameUsingLet)
 
-// function c(){
-//     var name = "praveen"
-// }
-// c();
 
-// console.log(b)
-// // console.log(name)
-// console.log(nameUsingLet);
-// console.log(bInsideIfCondition);
+
+
+
+// // if(true) {
+// //     var b = "qwerty"
+// //     let bInsideIfCondition = "eruoiewuroqwer"
+// // }
+
+// // for (let index = 0; index < 3; index++) {
+// //     setTimeout(()=>{ console.log(index) }, 0)
+// // }
+
+// // if(true)
+// //     {
+// //         var b = "ruweorwqiur"
+// //     }
+
+// // function c(){
+// //     var name = "praveen"
+// // }
+// // c();
+
+// // console.log(b)
+// // // console.log(name)
+// // console.log(nameUsingLet);
+// // console.log(bInsideIfCondition);

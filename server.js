ffb11d3d9043bfa2fe163bcb947a0003cdbde9ad
Admin <admin@example.com>
2022-08-16 06:18:13 +0100

    setup socketio.

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import HomepageRouter from "./router/homepage"
 import ProductsRouter from "./router/products"
 import UsersRouter from "./router/users"
 import UsersAPIRouter from "./router/api-user"
+import { createServer } from "http"
+
+// Socket.io
+import { Server } from "socket.io"
 
 import * as graphql from "express-graphql"
 
@@ -11,6 +15,8 @@ import "./utils/mongoose-db.mjs"
 import { Schema } from "./graphql/schema/query.mjs";
 
 const app = express();
+const server = createServer(app)
+const io = new Server(server)
 
 app.use(express.json())
 app.use(express.urlencoded( { extended : true } ))
@@ -37,6 +43,10 @@ app.use("/graphql", graphql.graphqlHTTP({
 }))
 
 
-app.listen(PORT, () => {
-  console.log(`Server started at ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server started at ${PORT}`);
+// });
+
+server.listen(PORT, ()=>{
+  console.log("Application started at 3000")
+})

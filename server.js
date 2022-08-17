ddcf2152acb4cbb9e15e8d3d1903e133c12f809e
Admin <admin@example.com>
2022-08-17 05:52:47 +0100

    Added userlist.

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
-import "dotenv/config"
+import "dotenv/config";
 import express from "express";
-import HomepageRouter from "./router/homepage"
-import ProductsRouter from "./router/products"
-import UsersRouter from "./router/users"
-import UsersAPIRouter from "./router/api-user"
-import { createServer } from "http"
+import HomepageRouter from "./router/homepage";
+import ProductsRouter from "./router/products";
+import UsersRouter from "./router/users";
+import UsersAPIRouter from "./router/api-user";
+import { createServer } from "http";
 
 // Socket.io
-import { Server } from "socket.io"
+import { Server } from "socket.io";
 
-import * as graphql from "express-graphql"
+import * as graphql from "express-graphql";
 
-import "./utils/mongoose-db.mjs"
+import "./utils/mongoose-db.mjs";
 import { Schema } from "./graphql/schema/query.mjs";
 
 const app = express();
-const server = createServer(app)
-const io = new Server(server)
+const server = createServer(app);
+const io = new Server(server);
 
-app.use(express.json())
-app.use(express.urlencoded( { extended : true } ))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-const PORT = process.env.PORT || 3000
-
-
+const PORT = process.env.PORT || 3000;
 
 // POST         Create
 // GET          Read
@@ -37,34 +35,35 @@ app.use("/products", ProductsRouter);
 app.use("/users", UsersRouter);
 app.use("/api/users", UsersAPIRouter);
 
-app.get("/chat", (req, res)=>{
-  res.render("chat")
-})
+app.get("/chat", (req, res) => {
+  res.render("chat");
+});
 
+app.use(
+  "/graphql",
+  graphql.graphqlHTTP({
+    graphiql: true,
+    schema: Schema,
+  })
+);
 
-app.use("/graphql", graphql.graphqlHTTP({
-    graphiql : true,
-    schema : Schema
-}))
-
-
-io.on("connection", (socket)=>{
-  socket.on("chat-message", (msg)=>{
+io.on("connection", (socket) => {
+  socket.on("chat-message", (msg) => {
     // console.log(`User sent message - ${msg}`)
-    
+
     // Group Chat with all users on server.
     // io.emit("message", msg)
 
     // Group chat - except the sender.
-    socket.broadcast.emit("message", msg)
+    socket.broadcast.emit("message", msg);
+  });
 
+  socket.on("login", (msg) => {
+    socket.data = { username : msg }
+    socket.broadcast.emit("newuser", msg);
+  });
+});
 
-
-  })
-  
-})
-
-
-server.listen(PORT, ()=>{
-  console.log("Application started at 3000")
-})
+server.listen(PORT, () => {
+  console.log("Application started at 3000");
+});
